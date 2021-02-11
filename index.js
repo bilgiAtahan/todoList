fc7c22ad21402e843cc380fba6f1f5c7eec2d70d
bilgi <bilgi_atahan@hotmail.com>
2021-02-12 02:26:30 +0300

    add character limited

--- index.js
+++ index.js
@@ -5,35 +5,40 @@ const deleteAll = document.querySelector("#deleteAll")
 const listContent = document.querySelectorAll(".listContent")
 
 send.onclick = () => {
-    let span = document.createElement("span")
-    let btn = document.createElement("BUTTON")
-    let listText = document.createElement("h3")
-    let img = document.createElement("img")
-    let date = document.createElement("span")
+    if (text.value.length < 48) {
+        let span = document.createElement("span")
+        let btn = document.createElement("BUTTON")
+        let listText = document.createElement("h3")
+        let img = document.createElement("img")
+        let date = document.createElement("span")
 
-    let today = new Date();
-    let monthOfToday = today.getMonth() + 1;
-    let dateOftext = today.getDate() + " " + monthOfToday + " " + today.getFullYear() + "- " +
-        today.getHours() + ":" + today.getMinutes();
+        let today = new Date();
+        let monthOfToday = today.getMonth() + 1;
+        let dateOftext = today.getDate() + " " + monthOfToday + " " + today.getFullYear() + "- " +
+            today.getHours() + ":" + today.getMinutes();
 
-    btn.className = "delete-btn"
-    span.className = "listContent"
-    img.src = "./deletebutton.svg"
-    date.textContent = dateOftext
-    date.className = "date"
+        btn.className = "delete-btn"
+        span.className = "listContent"
+        img.src = "./deletebutton.svg"
+        date.textContent = dateOftext
+        date.className = "date"
 
-    list.appendChild(span)
-    span.appendChild(listText);
-    span.appendChild(btn);
-    span.appendChild(img);
-    list.appendChild(date)
+        list.appendChild(span)
+        span.appendChild(listText);
+        span.appendChild(btn);
+        span.appendChild(img);
+        list.appendChild(date)
 
-    listText.textContent += text.value
-    text.value = ""
+        listText.textContent += text.value
+        text.value = ""
 
-    btn.onclick = () => {
-        list.removeChild(span)
-        list.removeChild(".date")
+        btn.onclick = () => {
+            list.removeChild(span)
+            list.removeChild(date)
+        }
+    }
+    else{
+        alert("You can write character until 48")
     }
 }
 
